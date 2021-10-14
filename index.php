b36157994f736bedc7a32fa221fc2c93a32d1db0
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-13 23:00:00 -0500

    Made the textarea read only

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div>
-        <textarea name="textBox" cols="200" rows="50"><?php include "update_text.php"?></textarea>
+        <textarea name="textBox" cols="200" rows="50" readOnly="True"><?php include "update_text.php"?></textarea>
     </div>  
     <form action="./send_message_to_file.php" method="POST">
         <label for="message">Message:</label>
