a9b7f317e4b9e38cbb86b778db444613711799cf
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-18 15:18:11 -0500

    Fixed the forms in signup and login pages

--- login_page.php
+++ login_page.php
@@ -3,11 +3,11 @@
 ?>
 
     <h2>Log In</h2>
-    <form action="includes/login_page.inc.php" method="POST"></form>
-    <input type="text" name="username" placeholder="Username"> <br>
-    <input type="text" name="password" placeholder="Password"> <br>
-    <button type="submit" name="submit">Log In</button>
-
+    <form action="includes/login_page.inc.php" method="POST">
+        <input type="text" name="username" placeholder="Username"> <br>
+        <input type="text" name="password" placeholder="Password"> <br>
+        <button type="submit" name="submit">Log In</button>
+    </form>
 <?php
     include_once "footer.php";
 ?>
--- signup_page.php
+++ signup_page.php
@@ -3,11 +3,12 @@
 ?>
 
     <h2>Sign Up</h2>
-    <form action="includes/signup_page.inc.php" method="POST"></form>
-    <input type="text" name="Name" placeholder="Name"> <br>
-    <input type="text" name="username" placeholder="Username"> <br>
-    <input type="text" name="password" placeholder="Password"> <br>
-    <button type="submit" name="submit">Sign Up</button>
+    <form action="./includes/signup_page.inc.php" method="POST">
+        <input type="text" name="Name" placeholder="Name"> <br>
+        <input type="text" name="username" placeholder="Username"> <br>
+        <input type="text" name="password" placeholder="Password"> <br>
+        <button type="submit" name="submit">Sign Up</button>
+    </form>
 
 <?php
     include_once "footer.php";
