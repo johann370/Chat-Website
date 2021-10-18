014dce0a819ee1ef9ecfc1a274da9e4c5066e4dc
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-18 16:25:01 -0500

    Created login functionality

--- includes/login_page.inc.php
+++ includes/login_page.inc.php
@@ -1,1 +1,18 @@
 <?php
+if(isset($_POST["submit"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    require_once "dbh.inc.php";
+    require_once "functions.inc.php";
+
+    if(emptyFieldsLogin($username, $password) !== false){
+        header("location: ../login_page.php?error=emptyfields");
+    }
+
+    loginUser($conn, $username, $password);
+
+}else{
+    header("location: ../login_page.php");
+    exit();
+}
