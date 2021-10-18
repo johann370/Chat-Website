fc63cea35ee1dd3431393e2e73a5a0b928527b17
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-18 14:40:38 -0500

    Created index.php for home page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="index.php">Home</li>
+        <li><a href="signup_page.php">Sign up</li>
+        <li><a href="login_page.php">Log in</li>
+    </ul>
+</body>
+</html>
