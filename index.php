50a83d466efa0e798a1b42786c5738b88abed222
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-18 14:43:07 -0500

    Created header and footer pages

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <ul>
-        <li><a href="index.php">Home</li>
-        <li><a href="signup_page.php">Sign up</li>
-        <li><a href="login_page.php">Log in</li>
-    </ul>
-</body>
-</html>
+<?php
+    include_once "header.php";
+?>
+
+
+<?php
+    include_once "footer.php";
+?>
