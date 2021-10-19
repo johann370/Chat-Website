7856e92b71e80455a1a1e7bd2e2c6239009e0f1d
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-19 14:28:18 -0500

    Added chat box when user is logged in

--- chat_page.php
+++ chat_page.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat Website</title>
-</head>
-<body>
-    <div>
-        <textarea name="textBox" cols="200" rows="50" readOnly="True"><?php include "update_text.php"?></textarea>
-    </div>  
-    <form action="./send_message_to_file.php" method="POST">
-        <label for="message">Message:</label>
-        <input type="text" name="message" id="message">
-        <input type="submit">
-    </form>
-</body>
-</html>
+<div>
+    <textarea name="textBox" cols="200" rows="50" readOnly="True"><?php include "update_text.php"?></textarea>
+</div>  
+<form action="./send_message_to_file.php" method="POST">
+     <label for="message">Message:</label>
+     <input type="text" name="message" id="message">
+     <input type="submit">
+ </form>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     include_once "header.php";
+    if(isset($_SESSION["user_username"])){
+        include_once "chat_page.php";
+    }
 ?>
-
 
 <?php
     include_once "footer.php";
